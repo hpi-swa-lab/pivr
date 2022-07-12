57f9bd8033e505e40af343df1ada7f1b05b3ec5b
Tom Beckmann <tomjonabc@gmail.com>
2022-07-12 19:16:33 +0200

    stuff

--- Provider.gd
+++ Provider.gd
@@ -1,12 +1,19 @@
 extends Node
 
+export(float, 0.1, 3) var block_scale = 1 setget set_block_scale
+
+func set_block_scale(value):
+	block_scale = value
+	print($"../Blocks")
+	$"../Blocks".scale = Vector2.ONE * block_scale
+
 func test_func():
 	print("Hellol")
 
 func doOpenEditorMorphCommand(structureOfBlockJson: String):
 	var blockStructure = JSON.parse(structureOfBlockJson).result
 	var block = buildBlock(blockStructure)
-	get_parent().add_child(block)
+	$"../Blocks".add_child(block)
 	block.rect_position.x = 0
 	block.rect_position.y = 0
 
