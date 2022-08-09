02085b65ac2b3217ae89ad117caeabc860edac24
leogeier <contact@leonardgeier.com>
2022-08-09 18:03:05 +0200

    add grabbable handle to system browser

--- project/mover/mover.gd
+++ project/mover/mover.gd
@@ -18,7 +18,9 @@ func _process(_delta):
 	var best = best_colliding_object()
 	var hovered_node = null
 	if best != null:
-		hovered_node = best.get_grabbed_node().get_root_block()
+		hovered_node = best.get_grabbed_node()
+		if hovered_node.has_method("get_root_block"):
+			hovered_node = hovered_node.get_root_block()
 	if hovered_node != null and hovered_node.has_method("on_hover_in"):
 		hovered_node.on_hover_in({"highlight_color_name": "blue"})
 	if hovered_node != last_hovered_node:
@@ -36,7 +38,9 @@ func _on_button_pressed(button):
 	var best = best_colliding_object()
 	if best == null:
 		return
-	grabbed_node = best.get_grabbed_node().get_root_block()
+	grabbed_node = best.get_grabbed_node()
+	if grabbed_node.has_method("get_root_block"):
+		grabbed_node = grabbed_node.get_root_block()
 	
 	var delta_transform = global_transform.inverse() * grabbed_node.global_transform
 	if grabbed_node.has_method("on_grab"):
